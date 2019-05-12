048743806456076837f291f05cc83b08887252a5
jsaka1212 <38971167+jsaka1212@users.noreply.github.com>
2019-05-12 15:05:31 -0400

    Add files via upload

--- App.js
+++ App.js
@@ -5,41 +5,46 @@ import picGunhoo from './Assets/gunhoo.jpg'
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'row',
-    alignItems: 'flex-start',
-    justifyContent: 'space-around',
-    backgroundColor: '#DDD'
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#F5FCFF'
   },
-  defaultText: {
-    textAlign: 'center',
-    fontSize: 22,
+  button: {
+    fontSize: 30,
+    margin: 10,
     padding: 10,
-    margin: 5,
-    color: 'black',
-    borderWidth: StyleSheet.hairlineWidth
-  },
-  selectedText: {
-    backgroundColor: 'yellow',
-    color: 'blue',
-    fontWeight: 'bold'
-  },
-  pic: {
-    borderRadius: 120
-  },
+    borderWidth: 2,
+    borderRadius: 10,
+    alignSelf: 'stretch',
+    textAlign: 'center'
+  }
 })
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <StatusBar hidden={true}/>
-        <Text style={styles.defaultText}>Sierra</Text>
-        <Text style={[styles.defaultText, styles.selectedText]}>Tanner</Text>
-        <Text style={styles.defaultText}>Travis</Text>
-        <Image style={styles.pic} source={picGunhoo}/>  
-      </View>
 
-    );
+  constructor() {
+    super()
+    this.state = {
+      backgroundColor: 'blue'
+    }
+  }
+
+  changeColor(backgroundColor) {
+    this.setState({backgroundColor})
+    this.changeColor = this.changeColor.bind(this)
+  }
+
+  render() {
+    const { backgroundColor } = this.state
+    return (
+      <View style={[
+        styles.container,{backgroundColor}]}>
+        <Text style={styles.button}
+          onPress={() => this.changeColor('green')}>Green</Text>
+        <Text style={styles.button}
+          onPress={() => this.changeColor('red')}>Red</Text>
+      </View>
+    )
   }
 }
 
