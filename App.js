923435f1071dd82a177af69a4f648955f212a94e
jsaka1212 <38971167+jsaka1212@users.noreply.github.com>
2019-05-12 14:52:20 -0400

    Add files via upload

--- App.js
+++ App.js
@@ -1,26 +1,46 @@
-import React, { Component } from 'react';
-import { AppRegistry, Text, TextInput, View } from 'react-native';
+import React from 'react'
+import { TextInput, Image, StyleSheet, AppRegistry, View, Text, StatusBar } from 'react-native'
+import picGunhoo from './Assets/gunhoo.jpg'
 
-export default class Amount extends Component {
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'flex-start',
+    justifyContent: 'space-around',
+    backgroundColor: '#DDD'
+  },
+  defaultText: {
+    textAlign: 'center',
+    fontSize: 22,
+    padding: 10,
+    margin: 5,
+    color: 'black',
+    borderWidth: StyleSheet.hairlineWidth
+  },
+  selectedText: {
+    backgroundColor: 'yellow',
+    color: 'blue',
+    fontWeight: 'bold'
+  },
+  pic: {
+    borderRadius: 120
+  },
+})
 
-  static navigationOptions = {
-
+export default class App extends React.Component {
   render() {
     return (
-      <View style={{padding: 10}}>
-        <TextInput
-          style={{height: 40}}
-          amt="Enter amount: $"
-          onChangeText={(text) => this.setState({text})}
-        />
-        <Text style={{padding: 10, fontSize: 42}}>
-          {this.state.text.split(' ').map((word) => 'You entered '&& word).join(' ')}
-        </Text>
+      <View style={styles.container}>
+        <StatusBar hidden={true}/>
+        <Text style={styles.defaultText}>Sierra</Text>
+        <Text style={[styles.defaultText, styles.selectedText]}>Tanner</Text>
+        <Text style={styles.defaultText}>Travis</Text>
+        <Image style={styles.pic} source={picGunhoo}/>  
       </View>
-    )
+
+    );
   }
-};
+}
 
-
-
-
+AppRegistry.registerComponent('BYM', () => App) 
