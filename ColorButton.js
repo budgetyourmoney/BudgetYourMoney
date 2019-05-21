2e174b95d80db9150f95d5b4ff9987749852cc8e
Saem Jeon <38971167+jsaka1212@users.noreply.github.com>
2019-05-20 22:52:01 -0400

    Create ColorButton.js

---
+++ ColorButton.js
@@ -0,0 +1,44 @@
+{/* Should locate under components directory as ColorButton.js */}
+import React from "react";
+import { StyleSheet, Text, View, TouchableHighlight } from "react-native";
+
+const styles = StyleSheet.create({
+  button: {
+    margin: 10,
+    padding: 10,
+    borderWidth: 2,
+    borderRadius: 10,
+    alignSelf: "stretch",
+    backgroundColor: "rgba(255, 255, 255, 0.8)"
+  },
+  row: {
+    flexDirection: "row",
+    alignItems: "center"
+  },
+  sample: {
+    height: 20,
+    width: 20,
+    borderRadius: 10,
+    margin: 5,
+    backgroundColor: "white"
+  },
+  text: {
+    fontSize: 30,
+    margin: 5
+  }
+});
+
+const ColorButton = ({ backgroundColor, onSelect = f => f }) => (
+  <TouchableHighlight
+    style={styles.button}
+    onPress={() => onSelect(backgroundColor)}
+    underlayColor="orange"
+  >
+    <View style={styles.row}>
+      <View style={[styles.sample, { backgroundColor }]} />
+      <Text style={styles.text}>{ backgroundColor }</Text>
+    </View>
+  </TouchableHighlight>
+);
+
+export default ColorButton;
